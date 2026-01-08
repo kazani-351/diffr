0d8885435ae693f25e08b8148f7b5c41caba2e54
Kazani <kazani@disroot.org>
2026-01-08 17:15:06 +0530

    Create ThemeToggle.svelte

---
+++ src/lib/ThemeToggle.svelte
@@ -0,0 +1,25 @@
+<script>
+  let isDark = false;
+
+  // Check current state
+  if (typeof document !== 'undefined') {
+    isDark = document.documentElement.classList.contains('dark');
+  }
+
+  function toggleTheme() {
+    isDark = !isDark;
+    if (isDark) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+</script>
+
+<button on:click={toggleTheme} aria-label="Toggle Dark Mode">
+  {#if isDark}
+    ☀️
+  {:else}
+    🌙
+  {/if}
+</button>
